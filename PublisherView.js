d9b330e007678d3ca32080155c72c67f8ba2501f
Vyacheslav Voronchuk <voronchuk@gmail.com>
2016-11-21 21:32:55 +0200

    Update PublisherView.js
    Missing component

--- PublisherView.js
+++ PublisherView.js
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { requireNativeComponent, View, findNodeHandle, TouchableHighlight, UIManager } from 'react-native';
+import { requireNativeComponent, View, Text, findNodeHandle, TouchableHighlight, UIManager } from 'react-native';
 import React from 'react';
 import SessionViewProps from './SessionViewProps';
 import withLoadingSpinner from './withLoadingSpinner';
